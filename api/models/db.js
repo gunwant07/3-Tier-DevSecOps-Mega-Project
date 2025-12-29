ff2251a0e8517560d0d4f05d22308bb0dda76a5c
gunwant07 <144123267+gunwant07@users.noreply.github.com>
2025-12-29 12:01:25 +0530

    Update db.js

--- api/models/db.js
+++ api/models/db.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
-const db = mysql
-  .createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+const db = mysql.createPool({
+  host: process.env.DB_HOST || 'mysql',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'crud_app',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = db;
